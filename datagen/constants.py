bb4fb574d38759bd3989d9ee1ea695b65766d2f2
sardonicism-04 <sardony01@gmail.com>
2023-01-20 16:17:39 -0500

    fix: update data URL

--- datagen/constants.py
+++ datagen/constants.py
@@ -3,7 +3,7 @@ from yarl import URL
 # Source for textures to be downloaded from
 TexturesBase = URL("https://upload-os-bbs.mihoyo.com/game_record/genshin")
 # Source for data files to be downloaded from
-DataFileBase = URL("https://raw.githubusercontent.com/Dimbreath/GenshinData/master/ExcelBinOutput")
+DataFileBase = URL("https://raw.githubusercontent.com/Sycamore0/GenshinData/main/ExcelBinOutput")
 
 # Map data stat names to GOOD stat keys
 STAT_MAPPING = {
