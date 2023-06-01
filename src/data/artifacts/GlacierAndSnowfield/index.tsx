0a7fe2c558dd031e40c22911c6685c8184571c61
sardonicism-04 <sardony01@gmail.com>
2023-06-01 12:45:03 -0500

    feat: update to 3.7 data

--- src/data/artifacts/GlacierAndSnowfield/index.tsx
+++ src/data/artifacts/GlacierAndSnowfield/index.tsx
@@ -1,8 +1,3 @@
-import goblet from './goblet.png'
-import plume from './plume.png'
-import circlet from './circlet.png'
-import flower from './flower.png'
-import sands from './sands.png'
 import data from "./data.json";
-const toExport = { goblet, plume, circlet, flower, sands, data };
+const toExport = { data };
 export default toExport;
